a1dd5bb487fb5e19d253a14a033fbc8e11670456
dmendres <dmendres@acm.org>
2015-06-19 14:49:10 -0500

    Implementation of RProgramming Assignment 2
    Implementation for peer review. NB: unit tests are commented out. Very
    much a clone of the example, but how else to do it?

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,41 @@
-## Put comments here that give an overall description of what your
-## functions do
+## CacheMatrix provides two functions for managing a cached matrix inverse computation
+### makeCacheMatrix creates the matrix cache, with an optional matrix
+### cacheSolve solves for (and caches) the inverse of the matrix cached in its argument
+### The cached value is used if the matrix hasn't been updated, and the 
 
-## Write a short comment describing this function
+## Creates a cache holding a matrix and its inverse, with functions to manage the state of the cache
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  mInv <- NULL
+  set <- function(y) {
+    x <<- y
+    mInv <<- NULL
+  }
+  get <- function() x
+  setInv <- function(inv) mInv <<- inv
+  getInv <- function() mInv
+  list(set = set, get = get, 
+       setInv = setInv, getInv = getInv)
 }
 
 
-## Write a short comment describing this function
+## Returns the inverse of a matrix stored in cache x, possibly computing it if the cached matrix has been updated.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  inv = x$getInv()
+  if (is.null(inv)) {
+    x$setInv(solve(x$get()))
+  }
+  x$getInv()  
 }
+
+#unit tests
+x <- makeCacheMatrix(matrix(1:4,2,2))
+cacheSolve(x)
+cacheSolve(makeCacheMatrix(cacheSolve(x)))
+m <- matrix()
+
+#non-trivial test
+x <- makeCacheMatrix(matrix(runif(100,max = 100), 10, 10))
+all.equal(cacheSolve(makeCacheMatrix(cacheSolve(x))),x$get())
